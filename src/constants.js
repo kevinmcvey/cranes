78c0d34e7b729a7c4c1f8dfc3f56bfc5e4a1c16a
kevinmcvey <mcveymk@gmail.com>
2022-02-03 23:58:16 -0800

    Move some constants into a file ... of constants

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  // To reduce "pop" sounds when gain adjusts, take this time to linearly adjust
+  GAIN_ADJUSTMENT_RAMP_MS: 16,
+
+  // Length of a morse "dit"
+  MORSE_INTERVAL_MS: 100,
+
+  // Size of the cranes image in pixels
+  IMAGE_WIDTH: 4032,
+  IMAGE_HEIGHT: 2548
+};
